c7f897699a7766498c23f0fc278ed095c40c0500
Mike Long <mlong@pivotal.io>
2016-04-11 18:43:55 -0700

    grabbing the service label

--- app.groovy
+++ app.groovy
@@ -14,7 +14,9 @@ class Application {
     model.addAttribute("application_name", env.getProperty("vcap.application.name", "@application_name"))
     model.addAttribute("app_instance_index", env.getProperty("vcap.application.instance_index", "@app_instance_index"))
     model.addAttribute("application_mem_limits", env.getProperty("vcap.application.limits.mem", "@application_mem_limits"))
-    model.addAttribute("vcap_services", env.getProperty("vcap.services")) // how do we get properties for a service when we don't necessarily know the name?
+    model.addAttribute("service_label", env.getProperty("vcap.services.p-mysql-db.label", "@service_label")) // how do we get properties for a service when we don't necessarily know the exact label property?
+    model.addAttribute("application_disk_limits", env.getProperty("vcap.application.limits.disk", "@application_disk_limits"))
+    model.addAttribute("app_space_name", env.getProperty("vcap.application.space_name", "@application_disk_limits"))
     return "index"
   }
 }
