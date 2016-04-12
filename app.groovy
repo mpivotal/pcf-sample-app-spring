c290a1b308b8e3834eb32ac9b4a40ff4f6d01868
Mike Long <mlong@pivotal.io>
2016-04-11 17:13:39 -0700

    adding add'l properties and inspecting the service

--- app.groovy
+++ app.groovy
@@ -9,11 +9,12 @@ class Application {
   private Environment env;
 
   @RequestMapping("/")
-  public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-    model.addAttribute("name", name)
+  public String index(Model model) {
     // For details of what's in the Environment see https://github.com/spring-projects/spring-boot/blob/master/spring-boot/src/main/java/org/springframework/boot/cloud/CloudFoundryVcapEnvironmentPostProcessor.java
-    model.addAttribute("application_name", env.getProperty("vcap.application.name", "none"))
-    model.addAttribute("app_instance_index", env.getProperty("vcap.application.instance_index"))
-    return "greeting"
+    model.addAttribute("application_name", env.getProperty("vcap.application.name", "@application_name"))
+    model.addAttribute("app_instance_index", env.getProperty("vcap.application.instance_index", "@app_instance_index"))
+    model.addAttribute("application_mem_limits", env.getProperty("vcap.application.limits.mem", "@application_mem_limits"))
+    model.addAttribute("vcap_services", env.getProperty("vcap.services")) // how do we get properties for a service when we don't necessarily know the name?
+    return "index"
   }
 }
